41083dca10042723bbd05a51c6b7237c98268bc4
RJDdev <rahnidemeis@gmail.com>
2020-07-08 20:25:54 +0200

    Delete react front end directory

--- front/src/utils/getFormattedDate.js
+++
@@ -1,8 +0,0 @@
-// format in MMM DD, YYYY (August 29, 2019)
-const getFormattedDate = date => {
-    return new Date(Date.parse(date)).toLocaleDateString("en-UK", {
-        dateStyle: "long"
-    })
-}
-
-export default getFormattedDate
