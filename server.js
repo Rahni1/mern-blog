76a43fe3598fc8438ba24d7563150955402c1ca0
RJDdev <rahnidemeis@gmail.com>
2020-10-21 10:49:54 +0200

    Connect mongodb database

--- server.js
+++ server.js
@@ -1,53 +1,61 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const expressValidator = require('express-validator')
-const path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+const path = require("path");
 
-require('dotenv').config()
+require("dotenv").config();
 
 // import routes
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const postRoutes = require('./routes/posts')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/posts");
 // app
-const app = express()
+const app = express();
 
 // connect db - first arg is url (specified in .env)
-mongoose.connect(process.env.ORMONGO_RS_URL, {
-useNewUrlParser: true,
-useCreateIndex: true,
-useUnifiedTopology: true,
-useFindAndModify: false
-}).then(() => console.log('DB connected'))
+const url = process.env.MONGODB_URI 
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.connection
+  .once("open", function () {
+    console.log("DB Connected!");
+  })
+  .on("error", function (error) {
+    console.log("Error is: ", error);
+  });
 
-// middlewares 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
 // used to save users credentials
-app.use(cookieParser())
-app.use(expressValidator())
-app.use(cors())
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
 
-app.use(authRoutes)
+app.use(authRoutes);
 // routes middleware
-app.use(userRoutes)
+app.use(userRoutes);
 // app.use(userRoutes)
-app.use(postRoutes)
+app.use(postRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-app.use(express.static( 'client/build' ))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
