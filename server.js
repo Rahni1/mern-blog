2a412cb58fd499411803e810387a4ddc2b26f11b
RJDdev <rahnidemeis@gmail.com>
2020-10-24 18:11:17 +0200

    Add '/' before '*' to avoid blank page on '/' route

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(postRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
