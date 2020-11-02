18c92f104f64112df4f5680c3d7c92d05f117e0a
RJDdev <rahnidemeis@gmail.com>
2020-11-02 14:35:48 +0100

    Modify code in app.get

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
