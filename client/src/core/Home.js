7145c780c27b00cdf645ebcb3437dc898cc2d3a4
RJDdev <rahnidemeis@gmail.com>
2020-11-04 14:44:50 +0100

    UI Design on about banner

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 import Navbar from './Navbar'
 import ListPosts from '../core/ListPosts';
-
+import diamond from "../img/diamond.png"
 const Home = ({props, user}) => {
 return (
         <div>
         <Navbar />
         <div className="background">
         <div className="about-app">
-        <h5><span>Shiny Syntax</span> is a blogging platform for developers and all those who enjoy tech to share & learn about new concepts,
+        <h5><span className="keywords">Shiny Syntax</span> is a blogging platform for developers and all those who enjoy tech to share & learn about new concepts,
          the latest technologies and writing shiny code. It's for everyone who's interested in tech but
          our aim is to provide somewhere where juniors can learn as well as share their knowledge 
-         where they're supported and encouraged to grow. 
-         Be a <span>diamond</span> and get blogging!💎
-         </h5>
+         where they're supported and encouraged to grow.
+         Be a <span className="keywords">diamond</span> and get blogging!   
+        <img className="diamond-icon" src={diamond} alt="diamond"
+         width="3.2%" height="3.2%" /> </h5>
         </div>
         </div>
         <ListPosts />
