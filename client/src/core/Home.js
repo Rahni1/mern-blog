684994dad5096db69f3d48e5ab53f9acc631140b
RJDdev <rahnidemeis@gmail.com>
2020-11-04 13:20:19 +0100

    Create about banner on home page

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -7,6 +7,16 @@ const Home = ({props, user}) => {
 return (
         <div>
         <Navbar />
+        <div className="background">
+        <div className="about-app">
+        <h5><span>Shiny Syntax</span> is a blogging platform for developers and all those who enjoy tech to share & learn about new concepts,
+         the latest technologies and writing shiny code. It's for everyone who's interested in tech but
+         our aim is to provide somewhere where juniors can learn as well as share their knowledge 
+         where they're supported and encouraged to grow. 
+         Be a <span>diamond</span> and get blogging!💎
+         </h5>
+        </div>
+        </div>
         <ListPosts />
     </div>
     )
